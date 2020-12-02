5ed4730e951102c7f64efbba686ebc613c98b50b
MikelLiza <mikellizarralde4@gmail.com>
2020-12-02 17:38:10 +0000

    First Commit

---
+++ src/COM2040_lab2-2.cpp
@@ -0,0 +1,19 @@
+/*
+ * COM2040_lab2-2.cpp
+ *
+ *  Created on: 12 Nov 2020
+ *      Author: mikel
+ */
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+	int a,b;
+	cin >> a >> b;
+	cout << int(sqrt(a*a + b*b) +0.5) << endl;
+	return 0;
+}
+
+
+
